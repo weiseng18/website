ca76e281565146f5b6a93de8398cfd5968c6ce14
weiseng18 <tws.hci@gmail.com>
2024-12-02 20:07:25 +0800

    change p to Box to allow elements inside the p

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,9 +2,14 @@ import type { MDXComponents } from 'mdx/types'
 import React from 'react'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
+  const Box = React.lazy(() =>
+    import('@chakra-ui/react').then((mod) => ({ default: mod.Box }))
+  )
   const ExternalLink = React.lazy(() => import('@components/ExternalLink'))
+
   return {
     a: ExternalLink,
+    p: (props) => <Box mb={4} {...props} />,
     ...components,
   }
 }
