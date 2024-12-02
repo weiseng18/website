fa0377678500ce870b25eebdf9be7eec2a99d024
weiseng18 <tws.hci@gmail.com>
2024-12-02 20:23:00 +0800

    update index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import { Spinner } from '@chakra-ui/react'
-import MainContainer from '../components/MainContainer'
+import { HStack, Image, Text, VStack } from '@chakra-ui/react'
+import MainContainer from '@components/MainContainer'
+import ExternalLink from '@components/ExternalLink'
+
+const SocialLink = ({ href, src, children }) => (
+  <HStack spacing={2}>
+    <Image src={src} boxSize="20px" />
+    <ExternalLink href={href} useExternalIcon={false}>
+      {children}
+    </ExternalLink>
+  </HStack>
+)
 
 export default function Index() {
   return (
     <MainContainer page="index">
-      <h1>Hello World!</h1>
-      <Spinner />
+      <VStack w="100%" justifyContent="center" spacing={8}>
+        <Text>Hi there!</Text>
+        <SocialLink href="https://github.com/weiseng18" src="github.svg">
+          Github
+        </SocialLink>
+      </VStack>
     </MainContainer>
   )
 }
