a5471560af640d0aeeac7ed5c5271004a20f0567
weiseng18 <tws.hci@gmail.com>
2024-12-03 00:22:37 +0800

    proj: add pixelart writeup

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,11 +5,13 @@ const Box = React.lazy(() =>
   import('@chakra-ui/react').then((mod) => ({ default: mod.Box }))
 )
 const ExternalLink = React.lazy(() => import('@components/ExternalLink'))
+const InternalLink = React.lazy(() => import('@components/InternalLink'))
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     a: ExternalLink,
     p: (props) => <Box mb={4} {...props} />,
+    InternalLink,
     ...components,
   }
 }
